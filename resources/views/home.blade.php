72b355e12f169b94222e492b9c6a51cbc7f6a672
Chris Su <chrissuvan@gmail.com>
2017-08-26 17:49:31 -0700

    Area selection

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="row">
+                    @foreach ($areas as $country)
+                        <div class="col-md-12">
+                            <h3><a href="">{{ $country->name }}</a></h3>
+                            <hr>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                            <div class="row">
+                                @foreach ($country->children as $state)
+                                    <div class="col-md-4">
+                                        <h4><a href="">{{ $state->name }}</a></h4>
+                                        <hr>
+                                        @foreach($state->children as $city)
+                                            <h5><a href="">{{ $city->name }}</a></h5>
+                                        @endforeach
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    @endforeach
                 </div>
             </div>
         </div>
