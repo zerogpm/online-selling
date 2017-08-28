2ca4e8f0d11940431c5248bf8cce5682d70ade69
Chris Su <chrissuvan@gmail.com>
2017-08-28 09:11:30 -0700

    Listing categories

---
+++ app/Http/Controllers/Category/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\{Area, Category};
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    public function index(Area $area)
+    {
+        $categories = Category::get()->toTree();
+
+        return view('categories.index', compact('categories'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,14 @@ Route::get('/', 'HomeController@index');
 Auth::routes();
 
 Route::get('/user/area/{area}', 'User\AreaController@store')->name('user.area.store');
+
+Route::group(['prefix' => '/{area}'], function() {
+
+    /**
+     * Category
+     */
+    Route::group(['prefix' => '/categories'], function() {
+        Route::get('/', 'Category\CategoryController@index')->name('category.index');
+    });
+
+});
