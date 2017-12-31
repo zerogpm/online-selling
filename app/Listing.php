e23b3cef5d9b14a6d6d13b12975fe18f8614aa78
Chris Su <zerogpm@gmail.com>
2017-12-31 12:44:26 -0800

    add favourite table

--- app/Listing.php
+++ app/Listing.php
@@ -58,4 +58,14 @@ class Listing extends Model
     {
         return $this->belongsTo(Area::class);
     }
+    
+    public function favourites()
+    {
+        return $this->morphToMany(User::class, 'favouriteable');
+    }
+
+    public function favouritedBy(User $user)
+    {
+        return $this->favourites->contains($user);
+    }
 }
